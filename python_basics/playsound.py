9d7ab3ee8b01b6cff344f5280ed07fd1b4c62e36
Semuel Leijten <semuel.leijten@hotmail.com>
2024-09-06 12:55:47 +0200

    added working playsound assignment

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,9 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("path/to/file.wav")
+wave_obj = sa.WaveObject.from_wave_file("python_basics/MP_140_F#_SynthLp1.wav")
 play_obj = wave_obj.play()
 play_obj.wait_done()
+
+# import simpleaudio.functionchecks as fc
+
+# fc.LeftRightCheck.run()
