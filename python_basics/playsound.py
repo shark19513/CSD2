7c8c5d84125f1b2af6d647999c2a5c53ae71a08f
Semuel Leijten <semuel.leijten@hotmail.com>
2024-09-09 20:30:38 +0200

    added user input to playsound.py, it doesn't work yet

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,8 +1,15 @@
 import simpleaudio as sa
+import time 
 
-wave_obj = sa.WaveObject.from_wave_file("python_basics/MP_140_F#_SynthLp1.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
+wave_obj = sa.WaveObject.from_wave_file("python_basics/boop.wav")
+
+boops = int(input ("How many boops? "))
+
+for i in range(boops):
+    print(f"Playing boop {i+1} of {boops}")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    # time.sleep(0.1)
 
 # import simpleaudio.functionchecks as fc
 
