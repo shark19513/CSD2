26e88df0312db49425e9b5973dc3eb9f491dabf2
Semuel Leijten <semuel.leijten@hotmail.com>
2024-11-30 17:15:15 +0100

    added soundingSine to the repo and created sounding_oscillators where i will remake it and add more waveforms

--- CSD2b/assignments/oscillator_Sem_rebuild/main.cpp
+++ CSD2b/assignments/oscillator_Sem_rebuild/main.cpp
@@ -4,12 +4,13 @@ int main() {
     std::cout << "\nin main\n";
     Synthesizer synth;
 
-    std::cout << "osc1 frequyency is: " << synth.osc1.getFrequency() << std::endl;
+    std::cout << "osc1 frequency is: " << synth.osc1.getFrequency() << std::endl;
     synth.osc1.setFrequency(216);
-    std::cout << "osc1 frequyency is: " << synth.osc1.getFrequency() << std::endl;
+    std::cout << "osc1 frequency is: " << synth.osc1.getFrequency() << std::endl;
 
     return 0;
 }
 
 // TODO: make keyboard setters and getters
 // TODO: use more setters and getters in general
+
